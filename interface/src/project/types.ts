2827e179117b95dd008d1175bf286e3dfe215ba1
heavy-matill <heavymatill@gmail.com>
2020-11-22 14:55:56 +0100

    added RGB type

--- interface/src/project/types.ts
+++ interface/src/project/types.ts
@@ -1,7 +1,18 @@
 export interface LightState {
   led_on: boolean;
 }
-
+export interface RGB {
+  cmd?: number;
+  mode?: number;
+  brightness: number
+  c1: Color;
+  c2: Color;
+}
+export interface Color {
+  r: number;
+  g: number;
+  b: number;
+}
 export interface LightMqttSettings { 
   unique_id : string;
   name: string;
